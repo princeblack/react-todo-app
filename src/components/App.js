512e53ec9c3204fa93f2ec5732827ebec5330fb7
princeblack <mahamadicongo@hotmail.com>
2019-12-17 10:45:05 +0100

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../css/App.scss';
+import Introtext from '../components/introtext'
+
+function App() {
+  return (
+    <div className="App">
+      i come from the APP component.
+      <Introtext/>
+    </div>
+  );
+}
+
+export default App;
